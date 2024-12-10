df22f8d385fc8e13681af63dc47aadaae3ee0017
nolanmai <khanhtrinh232005@gmail.com>
2024-12-09 20:48:57 -0700

    Added char

--- nolan-adventure/index.js
+++ nolan-adventure/index.js
@@ -12,6 +12,17 @@ const playerImage = new Image();
 playerImage.src = './img/playerDown.png'
 image.onload = () =>{
     c.drawImage(image, -750, -650);
-    c.drawImage(playerImage, canvas.width/2 - canvas.width/2, canvas.height/2);
+    c.drawImage(playerImage,
+        //for cropping 
+        0,
+        0,
+        playerImage.width/4,
+        playerImage.height,
+
+        canvas.width/2 - (playerImage.width/4)/2, canvas.height/2-playerImage.height/2,
+        playerImage.width/4,
+        playerImage.height
+    );
+
 }
 
